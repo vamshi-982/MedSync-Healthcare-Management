84449fc843f5fa0cf28e8952395fad0114d27c12
Siddapuram Vamshi <vamshisiddapuram982@gmail.com>
2026-03-11 23:40:16 +0530

    fixed swagger to use https for production

--- src/main/java/com/medsync/config/SwaggerConfig.java
+++ src/main/java/com/medsync/config/SwaggerConfig.java
@@ -1,33 +1,27 @@
 package com.medsync.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI medSyncOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("MedSync Healthcare Management API")
-                        .description("Secure REST API for managing patients, doctors, and appointments")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Siddapuram Vamshi")
-                                .email("vamshisiddapuram982@gmail.com")))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                        .description("REST API for Healthcare Management System")
+                        .version("1.0.0"))
+                .addServersItem(new Server()
+                        .url("https://medsync-healthcare-management-production.up.railway.app")
+                        .description("Production Server"))
+                .addServersItem(new Server()
+                        .url("http://localhost:8080")
+                        .description("Local Development"));
     }
 }
